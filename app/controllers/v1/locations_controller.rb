13f8fc5a7fcf38ef5ea0f7249ffd8f029cc534dc
Nikhil Lanjewar <nikhil@yournextleap.com>
2014-07-15 11:37:23 +0530

    Add permitted columns

--- app/controllers/v1/locations_controller.rb
+++ app/controllers/v1/locations_controller.rb
@@ -4,4 +4,9 @@ class V1::LocationsController < ApplicationController
   def api_resource
     Location
   end
+
+  def permitted_attributes
+    [:name, :parent_id, :latitude, :longitude, :type]
+  end
+
 end
