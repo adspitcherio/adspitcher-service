13d4fae52fa41fe526cdb0042e53767573aa9977
Nikhil Lanjewar <nikhil@yournextleap.com>
2014-08-20 22:54:14 +0530

    Debug database error

--- config/initializers/database_connection.rb
+++ config/initializers/database_connection.rb
@@ -4,6 +4,7 @@ Rails.application.config.after_initialize do
 
   ActiveSupport.on_load(:active_record) do
     if Rails.application.config.database_configuration
+      p Rails.application.config.database_configuration.inspect
       config = Rails.application.config.database_configuration[Rails.env]
       config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
       config['pool'] = ENV['DB_POOL'] || 20
